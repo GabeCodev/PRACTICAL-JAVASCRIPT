29f58bb4f39c3156d11540e79087ee38e19a6436
GabeCodev <gabriel.trenzadomartinez@gmail.com>
2020-06-29 16:40:46 -0600

    Created a variable displayTodosButton. Added event listener to the displayTodosButton.

--- script.js
+++ script.js
@@ -62,3 +62,9 @@ var todoList = {
       this.displayTodos();
   }
 };
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+
+displayTodosButton.addEventListener('click', function () {
+  todoList.displayTodos();
+});
